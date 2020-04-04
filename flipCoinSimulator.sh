04ae1b557a126cc2c533150231950792df17f670
Tushar-W <twankhede95@gmail.com>
2020-04-04 09:55:58 +0530

    ADDED:functions calculating singlet combination of flip coin and percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,16 +1,46 @@
 #!/bin/bash -x
 
-echo "Flip Coin Simulator"
+echo " Welcome To Flip Coin Simulator"
+declare -A singletCombination
+declare -A combinationPercentage
 
 function flipCoin() {
 	coin=$((RANDOM%2))
 	if [ $coin -eq 1 ];
 	then
-		echo "HEAD"
+		echo "H"
 	else
-		echo "TAIL"
+		echo "T"
 	fi
 }
 
-#call function for fliping coin
-flipCoin
+function storeSingletCombination() {
+	singletCombination["H"]=0
+	singletCombination["T"]=0
+
+	for((count=0; count<$1; count++))
+	do
+		result=$(flipCoin)
+		value=${singletCombination[$result]}
+		singletCombination[$result]=$((++value))
+	done
+
+}
+
+function getCombinationPercentage() {
+
+	for key in ${!singletCombination[@]}
+	do
+		value=${singletCombination[$key]}
+		combinationPercentage[$key]=$(($value*100/$1))
+	done
+}
+
+#store singlet combination
+storeSingletCombination 10
+echo ${singletCombination[@]}
+echo ${!singletCombination[@]}
+#calculate percentage of singlet combination
+getCombinationPercentage 10
+echo ${combinationPercentage[@]}
+echo ${!combinationPercentage[@]}
