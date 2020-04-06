ed916bf0cbe54007041f6565fe957d80eec0623e
Tushar-W <twankhede95@gmail.com>
2020-04-06 10:01:55 +0530

    ADDED:function for store doublet combination of flip coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,46 +1,78 @@
 #!/bin/bash -x
 
-echo " Welcome To Flip Coin Simulator"
+echo "Welcome To Flip Coin Simulator"
 declare -A singletCombination
-declare -A combinationPercentage
+declare -A doubletCombination
+declare -A percentage
+count=0
 
 function flipCoin() {
-	coin=$((RANDOM%2))
-	if [ $coin -eq 1 ];
-	then
-		echo "H"
-	else
-		echo "T"
-	fi
+      if [ $((RANDOM%2)) -eq 1 ];
+      then
+         echo "H"
+      else
+         echo "T"
+      fi
+}
+
+function getPercentage() {
+   for key in ${!singletCombination[@]}
+   do
+      value=${singletCombination[$key]}
+      percentage[$key]=$((value*100/numberOfTime))
+   done
 }
 
 function storeSingletCombination() {
-	singletCombination["H"]=0
-	singletCombination["T"]=0
-
-	for((count=0; count<$1; count++))
-	do
-		result=$(flipCoin)
-		value=${singletCombination[$result]}
-		singletCombination[$result]=$((++value))
-	done
-
+   singletCombination["H"]=0
+   singletCombination["T"]=0
+   while [ $count != $numberOfTime ]
+   do
+      result=$( flipCoin )
+      value=${singletCombination[$result]}
+      singletCombination[$result]=$((++value))
+      ((count++))
+   done
 }
 
-function getCombinationPercentage() {
-
-	for key in ${!singletCombination[@]}
-	do
-		value=${singletCombination[$key]}
-		combinationPercentage[$key]=$(($value*100/$1))
-	done
+function getDoubletPercentage() {
+   for key in ${!doubletCombination[@]}
+   do
+      value=${doubletCombination[$key]}
+      percentage[$key]=$((value*100/numberOfTime))
+   done
 }
 
-#store singlet combination
-storeSingletCombination 10
+function storeDoubletCombination() {
+   for((i=0; i<$numberOfTime; i++))
+   do
+      count=0
+      result=""
+      while [ $count != 2 ]
+      do
+         result+=$( flipCoin )
+         ((count++))
+      done
+      value=${doubletCombination[$result]}
+      doubletCombination[$result]=$((++value))
+   done
+}
+
+read -p "Enter Number Of Time:" numberOfTime
+storeSingletCombination
 echo ${singletCombination[@]}
 echo ${!singletCombination[@]}
-#calculate percentage of singlet combination
-getCombinationPercentage 10
-echo ${combinationPercentage[@]}
-echo ${!combinationPercentage[@]}
+getPercentage
+echo ${percentage[@]}
+echo ${!percentage[@]}
+storeDoubletCombination
+echo ${doubletCombination[@]}
+echo ${!doubletCombination[@]}
+getPercentage
+echo ${percentage[@]}
+echo ${!percentage[@]}
+getDoubletPercentage
+echo ${percentage[@]}
+echo ${!percentage[@]}
+
+
