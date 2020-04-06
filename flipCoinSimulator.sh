99c2dca025c68e244e57099eccf554470fdd4023
Tushar-W <twankhede95@gmail.com>
2020-04-06 10:12:10 +0530

    ADDED:function for get triplet combination of flip coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,19 +3,20 @@
 echo "Welcome To Flip Coin Simulator"
 declare -A singletCombination
 declare -A doubletCombination
+declare -A tripletCombination
 declare -A percentage
 count=0
 
 function flipCoin() {
-      if [ $((RANDOM%2)) -eq 1 ];
-      then
-         echo "H"
-      else
-         echo "T"
-      fi
+	if [ $((RANDOM%2)) -eq 1 ];
+   then
+   	echo "H"
+   else
+   	echo "T"
+   fi
 }
 
-function getPercentage() {
+function getSingletPercentage() {
    for key in ${!singletCombination[@]}
    do
       value=${singletCombination[$key]}
@@ -58,21 +59,46 @@ function storeDoubletCombination() {
    done
 }
 
+function getTripletPercentage() {
+   for key in ${!tripletCombination[@]}
+   do
+      value=${tripletCombination[$key]}
+      percentage[$key]=$((value*100/numberOfTime))
+   done
+}
+
+function storeTripletCombination() {
+   for((i=0; i<$numberOfTime; i++))
+   do
+      count=0
+      result=""
+		  while [ $count != 3 ]
+      do
+         result+=$( flipCoin )
+         ((count++))
+      done
+      value=${tripletCombination[$result]}
+      tripletCombination[$result]=$((++value))
+   done
+}
+
 read -p "Enter Number Of Time:" numberOfTime
 storeSingletCombination
 echo ${singletCombination[@]}
 echo ${!singletCombination[@]}
-getPercentage
+getSingletPercentage
 echo ${percentage[@]}
 echo ${!percentage[@]}
 storeDoubletCombination
 echo ${doubletCombination[@]}
 echo ${!doubletCombination[@]}
-getPercentage
-echo ${percentage[@]}
-echo ${!percentage[@]}
 getDoubletPercentage
 echo ${percentage[@]}
 echo ${!percentage[@]}
-
+storeTripletCombination
+echo ${tripletCombination[@]}
+echo ${!tripletCombination[@]}
+getTripletPercentage
+echo ${percentage[@]}
+echo ${!percentage[@]}
 
